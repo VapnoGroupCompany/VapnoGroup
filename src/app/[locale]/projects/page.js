0f7c5edaf67446cd4065d45154822d282875daaf
GregIvanchuk <gregiv99@gmail.com>
2024-03-31 19:02:52 +0300

    Add more quantity goods

--- src/app/[locale]/projects/page.js
+++ src/app/[locale]/projects/page.js
@@ -32,6 +32,27 @@ function Projects({ params: { locale } }) {
         { name:t("countbags"), value: t("good1countbagsValue") },
       ],
     },
+
+    {
+      title: t("good11Title"),
+      subTitle: t("good11Descr"),
+      applicationAreas: [
+        t("good11ListItem1"),
+        t("good11ListItem2"),
+        t("good11ListItem3"),
+        t("good11ListItem4"),
+        
+      ],
+      imageSrc: '/i6.png',
+      description: t("good11DetailDescr"),
+      characteristics: [
+        { name: t("country"), value: t("good11countryValue"), },
+        { name: t("producer"), value: t("good11producerValue")},
+        { name: t("weight"), value: t("good11weightValue") },
+        { name: t("wrap"), value: t("good11wrapValue") },
+        { name:t("countbags"), value: t("good11countbagsValue") },
+      ],
+    },
     {
       title: t("good2Title"),
       subTitle: t("good2Descr"),
@@ -41,7 +62,7 @@ function Projects({ params: { locale } }) {
         t("good2ListItem3"),
         t("good2ListItem4"),
       ],
-      imageSrc: '/i4.png',
+      imageSrc: '/i3.png',
       description: t("good2DetailDescr"),
       characteristics: [
         { name: t("country"), value: t("good2countryValue"), },
@@ -70,6 +91,106 @@ function Projects({ params: { locale } }) {
         { name:t("countbags"), value: t("good3countbagsValue") },
       ],
     },
+
+    {
+      title: t("good31Title"),
+      subTitle: t("good31Descr"),
+      applicationAreas: [
+        t("good31ListItem1"),
+        t("good31ListItem2"),
+        t("good31ListItem3"),
+        t("good31ListItem4"),
+      ],
+      imageSrc: '/i9.png',
+      description: t("good31DetailDescr"),
+      characteristics: [
+        { name: t("country"), value: t("good31countryValue"), },
+        { name: t("producer"), value: t("good31producerValue")},
+        { name: t("weight"), value: t("good31weightValue") },
+        { name: t("wrap"), value: t("good31wrapValue") },
+        { name:t("countbags"), value: t("good31countbagsValue") },
+      ],
+    },
+    {
+      title: t("good4Title"),
+      subTitle: t("good4Descr"),
+      applicationAreas: [
+        t("good4ListItem1"),
+        t("good4ListItem2"),
+        t("good4ListItem3"),
+        t("good4ListItem4"),
+      ],
+      imageSrc: '/i5.png',
+      description: t("good4DetailDescr"),
+      characteristics: [
+        { name: t("country"), value: t("good4countryValue"), },
+        { name: t("producer"), value: t("good4producerValue")},
+        { name: t("weight"), value: t("good4weightValue") },
+        { name: t("wrap"), value: t("good4wrapValue") },
+        { name:t("countbags"), value: t("good4countbagsValue") },
+      ],
+    },
+
+    {
+      title: t("good5Title"),
+      subTitle: t("good5Descr"),
+      applicationAreas: [
+        t("good5ListItem1"),
+        t("good5ListItem2"),
+        t("good5ListItem3"),
+        t("good5ListItem4"),
+      ],
+      imageSrc: '/i4.png',
+      description: t("good5DetailDescr"),
+      characteristics: [
+        { name: t("country"), value: t("good5countryValue"), },
+        { name: t("producer"), value: t("good5producerValue")},
+        { name: t("weight"), value: t("good5weightValue") },
+        { name: t("wrap"), value: t("good5wrapValue") },
+        { name:t("countbags"), value: t("good5countbagsValue") },
+      ],
+    },
+
+    {
+      title: t("good51Title"),
+      subTitle: t("good51Descr"),
+      applicationAreas: [
+        t("good51ListItem1"),
+        t("good51ListItem2"),
+        t("good51ListItem3"),
+        t("good51ListItem4"),
+      ],
+      imageSrc: '/i7.png',
+      description: t("good51DetailDescr"),
+      characteristics: [
+        { name: t("country"), value: t("good51countryValue"), },
+        { name: t("producer"), value: t("good51producerValue")},
+        { name: t("weight"), value: t("good51weightValue") },
+        { name: t("wrap"), value: t("good51wrapValue") },
+        { name:t("countbags"), value: t("good51countbagsValue") },
+      ],
+    },
+
+    {
+      title: t("good52Title"),
+      subTitle: t("good52Descr"),
+      applicationAreas: [
+        t("good52ListItem1"),
+        t("good52ListItem2"),
+        t("good52ListItem3"),
+        t("good52ListItem4"),
+      ],
+      imageSrc: '/i8.png',
+      description: t("good52DetailDescr"),
+      characteristics: [
+        { name: t("country"), value: t("good52countryValue"), },
+        { name: t("producer"), value: t("good52producerValue")},
+        { name: t("weight"), value: t("good52weightValue") },
+        { name: t("wrap"), value: t("good52wrapValue") },
+        { name:t("countbags"), value: t("good52countbagsValue") },
+      ],
+    },
+    
   ];
   
   return (
